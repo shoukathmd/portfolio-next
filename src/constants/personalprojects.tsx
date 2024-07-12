0063c4694d607e17142cf11c6d0842e8dc21f335
shoukathmd <shoukathmd@gmail.com>
2024-07-12 07:11:28 +0530

    Updated the finance project

--- src/constants/personalprojects.tsx
+++ src/constants/personalprojects.tsx
@@ -1,5 +1,23 @@
 export const personalProjectsData = [
   {
+    title: "Manage Finance",
+    description:
+      "A personal manage finance App to showcase my skills and projects.",
+    thumbnail: "/images/personal/turing/finance_app_1.png",
+    images: ["/images/personal/turing/finance_app_1.png"], // Add more image URLs if available
+    href: "https://finance-app-nine-delta.vercel.app/",
+    stack: [
+      "Next.js",
+      "React",
+      "Typescript",
+      "Tailwind CSS",
+      "Postgres",
+      "Drizzle ORM",
+    ],
+    content: "https://github.com/shoukathmd/finance-app.git",
+    slug: "finance-manager",
+  },
+  {
     title: "Jobify",
     description:
       "A personal Job Tracking App to showcase my skills and projects.",
@@ -20,35 +38,25 @@ export const personalProjectsData = [
     content: "https://github.com/shoukathmd/shadcn-dashboard",
     slug: "e-commerce-store",
   },
-  {
-    title: "Blog Platform",
-    description:
-      "A blog platform with markdown support and server-side rendering.",
-    thumbnail: "/images/personal/turing/perfai_1.png",
-    images: ["/images/personal/turing/perfai_1.png"], // Add more image URLs if available
-    href: "https://myblog.vercel.app",
-    stack: ["Next.js", "Markdown", "Prisma", "SQLite"],
-    content: "https://github.com/username/blog-platform",
-    slug: "blog-platform",
-  },
-  {
-    title: "Chat Application",
-    description: "A real-time chat application using WebSockets.",
-    thumbnail: "/images/personal/turing/perfai_1.png",
-    images: ["/images/personal/turing/perfai_1.png"], // Add more image URLs if available
-    href: "https://mychatapp.vercel.app",
-    stack: ["Next.js", "WebSockets", "Node.js"],
-    content: "https://github.com/username/chat-application",
-    slug: "chat-application",
-  },
-  {
-    title: "Task Manager",
-    description: "A task management app to track and organize your tasks.",
-    thumbnail: "/images/personal/turing/perfai_1.png",
-    images: ["/images/personal/turing/perfai_1.png"], // Add more image URLs if available
-    href: "https://taskmanager.vercel.app",
-    stack: ["Next.js", "React", "Firebase"],
-    content: "https://github.com/username/task-manager",
-    slug: "task-manager",
-  },
+  // {
+  //   title: "Blog Platform",
+  //   description:
+  //     "A blog platform with markdown support and server-side rendering.",
+  //   thumbnail: "/images/personal/turing/perfai_1.png",
+  //   images: ["/images/personal/turing/perfai_1.png"], // Add more image URLs if available
+  //   href: "https://myblog.vercel.app",
+  //   stack: ["Next.js", "Markdown", "Prisma", "SQLite"],
+  //   content: "https://github.com/username/blog-platform",
+  //   slug: "blog-platform",
+  // },
+  // {
+  //   title: "Chat Application",
+  //   description: "A real-time chat application using WebSockets.",
+  //   thumbnail: "/images/personal/turing/perfai_1.png",
+  //   images: ["/images/personal/turing/perfai_1.png"], // Add more image URLs if available
+  //   href: "https://mychatapp.vercel.app",
+  //   stack: ["Next.js", "WebSockets", "Node.js"],
+  //   content: "https://github.com/username/chat-application",
+  //   slug: "chat-application",
+  // },
 ];
